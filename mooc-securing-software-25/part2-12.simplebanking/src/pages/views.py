d7aae4db67aaff48c699480f48151880f9eb3868
i-douk <101892232+i-douk@users.noreply.github.com>
2025-05-22 10:08:21 +0100

    part2-12

--- mooc-securing-software-25/part2-12.simplebanking/src/pages/views.py
+++ mooc-securing-software-25/part2-12.simplebanking/src/pages/views.py
@@ -5,14 +5,30 @@ from django.contrib.auth.models import User
 from .models import Account
 from django.db.models import Q
 import json
-
-
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 @login_required
 def addView(request):
-	return redirect('/')
-
+    if request.method == 'POST':
+        try:
+            iban = request.POST.get('iban')
+            if not iban:
+                return JsonResponse({'error': 'IBAN is required'}, status=400)
+            
+            if Account.objects.filter(owner=request.user, iban=iban).exists():
+                return JsonResponse({'error': 'IBAN already exists'}, status=400)
+            
+            Account.objects.create(owner=request.user, iban=iban)
+            return redirect('/')
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    
 
 @login_required
 def homePageView(request):
-	return render(request, 'pages/index.html')
+
+	logged = request.user
+	accounts = Account.objects.filter(owner=logged)
+	print(logged)
+	return render(request, 'pages/index.html',{"accounts": accounts})
